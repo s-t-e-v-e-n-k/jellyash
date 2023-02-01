f14030b5ad4d2f6f372f85b44f85cfd1e7f05310
Steve Kowalik <steven@wedontsleep.org>
2023-02-01 14:35:02 +1100

    Add nextup module

---
+++ jellyash/nextup.py
@@ -0,0 +1,13 @@
+from .client import authed_client
+
+
+def episode_str(episode):
+    seasonindex = f"{episode.ParentIndexNumber}x{episode.IndexNumber:02}"
+    return f"{episode.SeriesName} [{seasonindex}] {episode.Name}"
+
+
+def nextup():
+    client = authed_client('jellyfin_nextup')
+    for episode in client.jellyfin.get_next(limit=30):
+        print(episode_str(episode))
+
