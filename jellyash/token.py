5d81d81dd71fc91b464e2f18a9e64b3b8f935526
Steve Kowalik <steven@wedontsleep.org>
2023-02-09 21:02:31 +1100

    Allow overridding of the username in token

--- jellyash/token.py
+++ jellyash/token.py
@@ -8,15 +8,16 @@ from .client import create_client, auth_with_password, CREDENTIALS_FILE
 def create_jellyfin_token():
     client = create_client('create_jellyfin_token')
     parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', dest='user', default=getuser())
     parser.add_argument(
         'server', nargs='?', default="https://jellyfin.wedontsleep.org/")
     args = parser.parse_args()
     password = getpassword()
-    result = auth_with_password(client, args.server, getuser(), password)
+    result = auth_with_password(client, args.server, args.user, password)
     if "AccessToken" in result:
         credentials = client.auth.credentials.get_credentials()
         server = credentials["Servers"][0]
-        server["username"] = getuser()
+        server["username"] = args.user
         with open(CREDENTIALS_FILE, 'w') as f:
             json.dump(server, f)
         print("Credentials saved.")
