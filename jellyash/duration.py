ea55c386de57b3c0376169623d7586da7fee5a51
Steve Kowalik <steven@wedontsleep.org>
2023-05-18 14:31:57 +0200

    Refactor duration

--- jellyash/duration.py
+++ jellyash/duration.py
@@ -5,16 +5,11 @@ from .client import authed_client
 from .search import search_single_show
 
 
-def average_duration() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("show", nargs="+")
-    args = parser.parse_args()
-    client = authed_client()
+def calculate_duration(client, show: str) -> str:
     try:
-        show = search_single_show(client, " ".join(args.show))
+        show = search_single_show(client, show)
     except ValueError as e:
-        print(str(e))
-        return
+        return str(e)
     count = 0
     duration = 0
     for season in client.jellyfin.get_seasons(show.Id):
@@ -22,4 +17,12 @@ def average_duration() -> None:
         episodes = client.jellyfin.get_season(show.Id, season.Id)
         duration += sum([e.RunTimeTicks for e in episodes])
     average = (duration / count) / int(Decimal("6E+008"))
-    print(f"Average duration over {count} episodes: {average:.1f} minutes")
+    return f"Average duration over {count} episodes: {average:.1f} minutes"
+
+
+def average_duration() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("show", nargs="+")
+    args = parser.parse_args()
+    client = authed_client()
+    print(calculate_duration(client, " ".join(args.show)))
