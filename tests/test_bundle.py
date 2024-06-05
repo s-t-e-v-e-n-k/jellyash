92758ed61c9cf00a15269164fa8df318e4a53d86
Steve Kowalik <steven@wedontsleep.org>
2024-06-05 22:37:31 +1000

    Refactor title checks into a method in bundle

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -28,6 +28,19 @@ class TestApiResponse(ClientTest):
         self.search_result = self.test_client.jellyfin.search_media_items(
             term="", media="Movies", limit=5
         )
+        self.names = [
+            "Blender Foundation",
+            "18 - The Passenger of the 'Atlanta'",
+            "14 - Pickaxe and Trowel",
+            "31 - Part II, Chapter 26: Bow and Arrow",
+            "22 - The New Citizen of the United States",
+        ]
+
+    def assert_item_titles(self, items, ids):
+        self.assertEqual(len(items), 2)
+        self.assertSequenceEqual(
+            [m.Name for m in items], [self.names[x] for x in ids]
+        )
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
     @pytest.mark.vcr
@@ -37,7 +50,7 @@ class TestApiResponse(ClientTest):
         self.assertEqual(len(self.search_result), 95)
         self.assertCountEqual(
             self.search_result._raw_value().keys(),
-            ["Items", "TotalRecordCount", "StartIndex"]
+            ["Items", "TotalRecordCount", "StartIndex"],
         )
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
@@ -53,51 +66,32 @@ class TestApiResponse(ClientTest):
     @pytest.mark.vcr
     def test_getitem_slice_start_end_no_jump(self):
         items = self.search_result[1:3]
-        self.assertEqual(len(items), 2)
-        self.assertEqual(
-            items[0].Name, "18 - The Passenger of the 'Atlanta'")
-        self.assertEqual(items[1].Name, "14 - Pickaxe and Trowel")
+        self.assert_item_titles(items, [1, 2])
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
     @pytest.mark.vcr
     def test_getitem_slice_start_end_with_jump(self):
         items = self.search_result[1:4:2]
-        self.assertEqual(len(items), 2)
-        self.assertEqual(
-            items[0].Name, "18 - The Passenger of the 'Atlanta'")
-        self.assertEqual(
-            items[1].Name, "31 - Part II, Chapter 26: Bow and Arrow")
+        self.assert_item_titles(items, [1, 3])
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
     @pytest.mark.vcr
     def test_getitem_slice_only_end(self):
         items = self.search_result[:2]
-        self.assertEqual(len(items), 2)
-        self.assertEqual(items[0].Name, "Blender Foundation")
-        self.assertEqual(
-            items[1].Name, "18 - The Passenger of the 'Atlanta'")
+        self.assert_item_titles(items, [0, 1])
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
     @pytest.mark.vcr
     def test_getitem_slice_only_start(self):
         items = self.search_result[3:]
-        self.assertEqual(len(items), 2)
-        self.assertEqual(
-            items[0].Name, "31 - Part II, Chapter 26: Bow and Arrow")
-        self.assertEqual(
-            items[1].Name, "22 - The New Citizen of the United States")
+        self.assert_item_titles(items, [3, 4])
 
     @pytest.mark.default_cassette("TestApiResponse.setUp.yaml")
     @pytest.mark.vcr
     def test_iter(self):
-        names = [
-            "Blender Foundation",
-            "18 - The Passenger of the 'Atlanta'",
-            "14 - Pickaxe and Trowel",
-            "31 - Part II, Chapter 26: Bow and Arrow",
-            "22 - The New Citizen of the United States"
-        ]
-        self.assertSequenceEqual([m.Name for m in self.search_result], names)
+        self.assertSequenceEqual(
+            [m.Name for m in self.search_result], self.names
+        )
 
 
 class TestUnwrappedApiResponse(ClientTest):
@@ -111,4 +105,3 @@ class TestUnwrappedApiResponse(ClientTest):
     def test_unwrapped_bool_response(self):
         resp = self.test_client.jellyfin._get("QuickConnect/Enabled")
         self.assertFalse(resp)
-
